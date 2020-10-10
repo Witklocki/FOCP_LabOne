c70a9068b933923482734d648aa8f2ff99ab4707
Witklocki <57358443+Witklocki@users.noreply.github.com>
2020-10-10 13:05:03 +0200

    loop for to show sin of i

--- loops.cpp
+++ loops.cpp
@@ -1,39 +1,43 @@
-#include<iostream>
+#include <iostream>
+#include <cmath>
 
     using namespace std;
 
 int main(){
 
-    int A;
+    // int A;
 
-    cout << "give A number" << endl;
-    cin >> A;
+    // cout << "give A number" << endl;
+    // cin >> A;
 
-    for (int i = 0; i < A ; i++){
-        cout << i+1 <<". Hello" << endl;
+    // for (int i = 0; i < A ; i++){
+    //     cout << i+1 <<". Hello" << endl;
+    // }
+
+    // int money;
+    // cout << "How much money you need" << endl; 
+    // cin >> money;
+    // int banknots = 0;
+    // while(money >= 50){
+    //     money = money - 50;
+    //     banknots++;
+    // }
+    // cout << "number of banknots =  " << banknots << endl;
+
+    // int banknotValue = 0;
+    // cout << "banknot value" << endl;
+    // cin >> banknotValue;
+    // cout << "How much money you need" << endl; 
+    // cin >> money;
+    // int banknot;
+
+    // for(banknot = 0; money >= banknotValue; banknot++){
+    //     money -= banknotValue;
+    // }
+    // cout << banknot;
+    for(int i = 0; i <= 360; i++){
+        cout << "angle : " << i << " = sin" << sin(i) << endl;
     }
-
-    int money;
-    cout << "How much money you need" << endl; 
-    cin >> money;
-    int banknots = 0;
-    while(money >= 50){
-        money = money - 50;
-        banknots++;
-    }
-    cout << "number of banknots =  " << banknots << endl;
-
-    int banknotValue = 0;
-    cout << "banknot value" << endl;
-    cin >> banknotValue;
-    cout << "How much money you need" << endl; 
-    cin >> money;
-    int banknot;
-
-    for(banknot = 0; money >= banknotValue; banknot++){
-        money -= banknotValue;
-    }
-    cout << banknot;
 
 return 0;
 
