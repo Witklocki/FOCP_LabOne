97ebc6af99ef59588192f0992979e53fde150152
Witklocki <57358443+Witklocki@users.noreply.github.com>
2020-10-10 12:55:49 +0200

    loop while change to loop for
    All things still are there

--- loops.cpp
+++ loops.cpp
@@ -21,7 +21,19 @@ int main(){
         money = money - 50;
         banknots++;
     }
-    cout << "number of banknots " << banknots << endl;
+    cout << "number of banknots =  " << banknots << endl;
+
+    int banknotValue = 0;
+    cout << "banknot value" << endl;
+    cin >> banknotValue;
+    cout << "How much money you need" << endl; 
+    cin >> money;
+    int banknot;
+
+    for(banknot = 0; money >= banknotValue; banknot++){
+        money -= banknotValue;
+    }
+    cout << banknot;
 
 return 0;
 
