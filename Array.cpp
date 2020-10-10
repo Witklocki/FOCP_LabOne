d236817e18f5eaf2a398fd18d3ee6f88ded0f6ce
Witklocki <57358443+Witklocki@users.noreply.github.com>
2020-10-10 13:35:41 +0200

    finding smalest value in array

--- Array.cpp
+++ Array.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 #include <array>
+#include <cstdlib>
+
+
 using namespace std;
 int main(){
 
     array<int, 5> data = { 0, 1, 2, 3, 4};
     array<int, 100> data2 ;
-    data2[0] = 9999;
-    for(int i = 1; i<10; i++){
-        data2[i] = i;
+    for(int i = 0; i<10; i++){
+        data2[i] = rand() % 100+i;
     }
+    int smalest = data2[0];
+    for(int i = 0; i < 10; i++){
+        if (data2[i] < smalest){
+            smalest = data2[i];
+        }
+    }
+    cout << "smalest is " << smalest << endl <<endl ;
     for(int i = 0; i<10; i++){
         cout << data2[i] <<endl;
     }
