b7a481197257e7b64a8d77fb4ab511f353ec7b7e
Witklocki <57358443+Witklocki@users.noreply.github.com>
2020-10-10 11:53:37 +0200

    even/odd number print
    even/odd number print

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -6,7 +6,14 @@ int main(){
 
     int myNumber;
     cout << "Hello World!!" << endl;
+    cout << "Give number" << endl;
     cin >> myNumber;
+    if(myNumber%2){
+        cout << " odd ";
+    }
+    else{
+        cout << "even ";
+    }
     cout << myNumber;
 
 
