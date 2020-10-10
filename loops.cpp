f1116bccdf86e6fd52da258f9258a8dc981cd94e
Witklocki <57358443+Witklocki@users.noreply.github.com>
2020-10-10 12:45:20 +0200

    add while loop

--- loops.cpp
+++ loops.cpp
@@ -6,13 +6,22 @@ int main(){
 
     int A;
 
-    cout << "give A can be negative" << endl;
+    cout << "give A number" << endl;
     cin >> A;
 
     for (int i = 0; i < A ; i++){
         cout << i+1 <<". Hello" << endl;
     }
 
+    int money;
+    cout << "How much money you need" << endl; 
+    cin >> money;
+    int banknots = 0;
+    while(money >= 50){
+        money = money - 50;
+        banknots++;
+    }
+    cout << "number of banknots " << banknots << endl;
 
 return 0;
 
