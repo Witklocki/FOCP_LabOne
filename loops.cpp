c4b160db2d169e6eea0f3cef992cef0647dc028d
Witklocki <57358443+Witklocki@users.noreply.github.com>
2020-10-10 12:33:35 +0200

    For loop

---
+++ loops.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+    using namespace std;
+
+int main(){
+
+    int A;
+
+    cout << "give A can be negative" << endl;
+    cin >> A;
+
+    for (int i = 0; i < A ; i++){
+        cout << i+1 <<". Hello" << endl;
+    }
+
+
+return 0;
+
+}
